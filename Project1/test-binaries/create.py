59105a28e741ae272f50bebbfd4addb802f22e79
kvv1618 <varun.kesharaju@gmail.com>
2025-02-18 18:27:18 -0500

    add project report

--- Project1/test-binaries/create.py
+++ Project1/test-binaries/create.py
@@ -3,14 +3,14 @@ import os
 import random
 
 value = random.getrandbits(64)
-filename = "binary_datafile_0002.dat"
+filename = "binary_datafile_26.dat"
 with open(filename, "wb") as f:
     while True:
         # Write the 64-bit unsigned integer in little-endian format
         f.write(struct.pack('<Q', value))
         value = random.getrandbits(64)
 
-        if os.stat(filename).st_size >= 0.002 * 1024 * 1024 * 1024:
+        if os.stat(filename).st_size >= 2.6 * 1024 * 1024 * 1024:
             break
 
 print(f"File '{filename}' created.")
